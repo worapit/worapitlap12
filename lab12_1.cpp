7bc0ee887082f5891aea0b40b638af2956085608
worapit <drm.wmwork@gmail.com>
2021-12-20 07:17:56 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    cout << "Press Enter 3 times to reveal your future." ;
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    int x = rand()%9;
+
+    cout <<"You will get " << grade[x] << " in this 261102." ;
+
+    return 0;
+
+}
+
+
+
+
+
+
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
